5d6c0d35a500654f8b724acb33ff8ef487fcac37
ansari <makansari.android@gmail.com>
2023-07-10 12:58:08 +0530

    class n object

--- main.py
+++ main.py
@@ -1,81 +1,103 @@
-
-# print("hello world")
+# """
+# # print("hello world")
+# #
+# # name = "abc"
+# # age = 44
+# #
+# # print("my name is " + name)
+# #
+# # college = "ams"
+# # a = 10
+# # b = 4
+# # c = 11
+# # if a<b:
+# #     print("bis greater")
+# #     if c>a:
+# #         print("c is greater")
+# # else:
+# #     print("a is greater")
+# #
+# # for i in range (1,10):
+# #     print(i)
+# #
+# # # mutable or immutable,  ordered or unordered,  duplicate or no duplicate
+# # #List (mutable, ordered, duplicates)   dictionary
+# #
+# # mylist = [3,4,33,5,6, 3]
+# # print(mylist[0])
+# # mylist.insert(0,100)
+# # mytup= {4,5,6,7,8}
+# #
+# # print(mylist)
+# #
+# # # mydictionary = [{"name": "ansair", "id":101},
+# # #                 {"name": "abc", "id":102}]
+# #
+# # mydictionary = [{"name": "ansair", "id1":101},
+# #                 {"name1": "ansair", "id2":101},
+# #                 {"name2": "ansair", "id3":101}
+# #                 ]
+# #non parmaterize fun
+# """
+# def playing():
+#     print("im playing")
+#     print("im running")
 #
-# name = "abc"
-# age = 44
+# playing()
 #
-# print("my name is " + name)
+# #parameterize function
+# def eating(food):
+#     print("i like this " + food)
 #
-# college = "ams"
-# a = 10
-# b = 4
-# c = 11
-# if a<b:
-#     print("bis greater")
-#     if c>a:
-#         print("c is greater")
-# else:
-#     print("a is greater")
+# eating("pizza")
 #
-# for i in range (1,10):
-#     print(i)
+# #return type
 #
-# # mutable or immutable,  ordered or unordered,  duplicate or no duplicate
-# #List (mutable, ordered, duplicates)   dictionary
+# def sum(a,b):
+#     res = a + b
+#     return  res
 #
-# mylist = [3,4,33,5,6, 3]
-# print(mylist[0])
-# mylist.insert(0,100)
-# mytup= {4,5,6,7,8}
+# print(sum(4,5))
+# """
 #
-# print(mylist)
+# #default parameters
+# def getDetails(name = "abc", age = 100):
 #
-# # mydictionary = [{"name": "ansair", "id":101},
-# #                 {"name": "abc", "id":102}]
+#     print(f"my name is {name} and my age is {age}")
+#
+#
+# getDetails("ansair", 33)
+# getDetails()
+# getDetails("ravi")
+# getDetails(age = 55, name="ramesh")
+#
 #
 # mydictionary = [{"name": "ansair", "id1":101},
-#                 {"name1": "ansair", "id2":101},
-#                 {"name2": "ansair", "id3":101}
+#                 {"name1": "ravi", "id2":102},
+#                 {"name2": "ramesh", "id3":103}
 #                 ]
-#non parmaterize fun
-"""
-def playing():
-    print("im playing")
-    print("im running")
+#
+# """
 
-playing()
-
-#parameterize function
-def eating(food):
-    print("i like this " + food)
-
-eating("pizza")
-
-#return type
-
-def sum(a,b):
-    res = a + b
-    return  res
-
-print(sum(4,5))
-"""
-
-#default parameters
-def getDetails(name = "abc", age = 100):
-
-    print(f"my name is {name} and my age is {age}")
-
-
-getDetails("ansair", 33)
-getDetails()
-getDetails("ravi")
-getDetails(age = 55, name="ramesh")
-
-
-mydictionary = [{"name": "ansair", "id1":101},
-                {"name1": "ravi", "id2":102},
-                {"name2": "ramesh", "id3":103}
-                ]
+class Tiger:
+    name = ""
+    age = 0
+    def __init__(self, age):
+        self.age = age
+        n = self.name
+        print("my constructor")
+        print(f"my tiger is {n} and {age}")
 
 
 
+    def playing(self):
+        print(f"my name is {self.name}")
+        print(f"my age is {self.age}")
+
+
+tiger = Tiger(44)
+print(tiger.name)
+
+tiger.playing()
+
+print(dir(tiger))
